08aeb2b96acb5e44f32c851477c7b65a1b5fd0e6
liuhuapiaoyuan <278780765@qq.com>
2023-03-23 14:23:21 +0800

    调整优先

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,9 +1,10 @@
-export const defaultAPIEndpoint =
-  'https://sharegpt.churchless.tech/share/v1/chat';
 export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
 
 export const availableEndpoints = [
+  'https://closeai.deno.dev/v1/chat/completions',
   'https://sharegpt.churchless.tech/share/v1/chat',
   'https://chatgpt-api.shn.hk/v1/',
   officialAPIEndpoint,
 ];
+
+export const defaultAPIEndpoint = availableEndpoints[0];
